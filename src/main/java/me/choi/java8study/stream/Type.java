e25b1aeac253458ee62c76179107f28d9ebdcd47
junwoo <33277588+mike6321@users.noreply.github.com>
2021-03-09 16:29:55 +0900

    create Dish dto

---
+++ src/main/java/me/choi/java8study/stream/Type.java
@@ -0,0 +1,12 @@
+package me.choi.java8study.stream;
+
+/**
+ * Project : java8study
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 4:27 오후
+ */
+public enum Type {
+    MEAT, FISH, OTHER
+}
