7c53a76509919171ae4afeb61c102fd7c2b884eb
junwoo <33277588+mike6321@users.noreply.github.com>
2021-03-14 20:07:48 +0900

    Optional의 활용

--- src/main/java/me/choi/java8study/optional/OptionalExample.java
+++ src/main/java/me/choi/java8study/optional/OptionalExample.java
@@ -23,15 +23,51 @@ public class OptionalExample {
     public static void main(String[] args) {
         init();
 
+        // TODO: Optional - isPresent 2021/03/14 7:51 오후
         Optional<OnlineClass> spring = springClasses.stream()
                                                     .filter(oc -> oc.getTitle().startsWith("spring"))
                                                     .findFirst();
 
+        boolean present = spring.isPresent();
+        System.out.println(present);
 
+        // TODO: Optional - get 2021/03/14 7:52 오후
+        /**
+         * return 이 Optional Type이 아니다.
+         * 가급적이면 get을 사용하지 말자!
+         * */
+        if (spring.isPresent()) {
+            OnlineClass onlineClass = spring.get();
+            System.out.println(onlineClass.getTitle());
+        }
 
+        // TODO: Optional - ifPresent 2021/03/14 7:57 오후
+        System.out.println("***************ifPresent***************");
+        spring.ifPresent(OnlineClass::getTitle);
 
+        // TODO: orElse 2021/03/14 7:59 오후
+        /**
+         * 있던 없던 무조건 탄다.
+         * */
+        System.out.println("***************orElse***************");
+        spring.orElse(createNewClasee());
+
+        // TODO: orElseGet 2021/03/14 8:01 오후
+        /**
+         * 없을때만 탄다.
+         * */
+        System.out.println("***************orElseGet***************");
+        spring.orElseGet(() -> createNewClasee());
+
+        OnlineClass onlineClass = spring.orElseThrow();
 
     }
+
+    private static OnlineClass createNewClasee() {
+        System.out.println("create New Class");
+        return new OnlineClass(10, "New Class", false);
+    }
+
     private static void init() {
         springClasses.add(new OnlineClass(1, "spring boot", true));
         springClasses.add(new OnlineClass(2, "spring data jpa", true));
